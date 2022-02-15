75d38149e48b27b270997a17645a4446194d6fc6
Dan <95330216+LifesABeach210@users.noreply.github.com>
2022-02-15 01:47:48 -0500

    Add files via upload

---
+++ future.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync');
+/*let age = (30);
+let old = (5);
+let a = age+old;
+console.log("i will be this old in 5 year"+a)*/
+/*
+let num1 = 5;
+let num2 = 4;
+let z = num1+num2;
+console.log(num1, "and", num2, "=",+z);*/
+/*
+let name = "dan";
+console.log("hello"+name);
+*/
+
+let a = 9;
+let b =5;
+let c = (a%5);
+
+console.log("the remainder of 9 from 5 is"+c);
